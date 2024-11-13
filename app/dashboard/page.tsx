982bb42cb0cd0ef2d73e2843f27c75d2d508a9f7
Matt Plant <matthew.plant@okta.com>
2024-11-13 14:20:29 +0000

    updated dashboard landing page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,13 @@ import Link from "next/link"
 import { ArrowRightIcon } from "@radix-ui/react-icons"
 
 import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default async function DashboardHome() {
   return (
@@ -9,30 +16,26 @@ export default async function DashboardHome() {
       <div className="flex flex-1 items-center justify-center rounded-3xl border bg-field shadow-sm">
         <div className="flex max-w-[500px] flex-col items-center gap-1 text-center">
           <h3 className="text-2xl font-bold tracking-tight">
-            Explore the SaaS Starter
+            Welcome to the MyCosta Portal
           </h3>
-          <p className="mt-3 text-muted-foreground">
-            This reference app demonstrates how to build a multi-tenant B2B SaaS
-            application powered by Auth0 by Okta.
-          </p>
-          <p className="mt-3 text-muted-foreground">
-            Head over to the Settings Dashboard to explore common administrative
-            capabilities like membership management, single sign-on
-            configuration, and security policies.
-          </p>
-          <div className="mt-8">
+        </div>
+        <Card className="mx-auto max-w-sm">
+          <CardHeader>
+            <CardTitle className="grid gap-2">
+              <span>Settings</span>
+            </CardTitle>
+            <CardDescription>
+              Manage your organization settings & members
+            </CardDescription>
+          </CardHeader>
+          <CardFooter>
             <Link href="/dashboard/organization/general" className="w-full">
               <Button className="w-full">
                 Navigate to Settings
-                <ArrowRightIcon className="ml-2 size-4" />
               </Button>
             </Link>
-          </div>
-          <p className="mt-3 text-muted-foreground">
-            (You must be logged in with an &quot;admin&quot; role in your
-            organization.)
-          </p>
-        </div>
+          </CardFooter>
+        </Card>
       </div>
     </div>
   )
