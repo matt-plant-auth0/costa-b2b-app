95aef26d0f272b602f0751d9b72fd36ccb849c2e
Matt Plant <matthew.plant@okta.com>
2024-11-13 14:40:29 +0000

    css rows added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -14,62 +14,64 @@ export default async function DashboardHome() {
   return (
     <div className="flex flex-1 flex-grow flex-col gap-4 lg:gap-6">
       <div className="flex flex-1 items-center justify-center rounded-3xl border bg-field shadow-sm">
-        <div className="flex max-w-[500px] flex-col items-center gap-1 text-center">
-          <h3 className="text-2xl font-bold tracking-tight">
-            Welcome to the MyCosta Portal
-          </h3>
+        <div className="grid grid-flow-row-dense grid-cols-3">
+          <div className="flex max-w-[500px] flex-col items-center gap-1 text-center">
+            <h3 className="text-2xl font-bold tracking-tight">
+              Welcome to the MyCosta Portal
+            </h3>
+          </div>
+          <Card className="mx-auto max-w-sm">
+            <CardHeader>
+              <CardTitle className="grid gap-2">
+                <span>Settings</span>
+              </CardTitle>
+              <CardDescription>
+                Manage your organization settings & members
+              </CardDescription>
+            </CardHeader>
+            <CardFooter>
+              <Link href="/dashboard/organization/general" className="w-full">
+                <Button className="w-full">
+                  Navigate to Settings
+                </Button>
+              </Link>
+            </CardFooter>
+          </Card>
+          <Card className="mx-auto max-w-sm">
+            <CardHeader>
+              <CardTitle className="grid gap-2">
+                <span>Settings</span>
+              </CardTitle>
+              <CardDescription>
+                Manage your organization settings & members
+              </CardDescription>
+            </CardHeader>
+            <CardFooter>
+              <Link href="/dashboard/organization/general" className="w-full">
+                <Button className="w-full">
+                  Navigate to Settings
+                </Button>
+              </Link>
+            </CardFooter>
+          </Card>
+          <Card className="mx-auto max-w-sm">
+            <CardHeader>
+              <CardTitle className="grid gap-2">
+                <span>Settings</span>
+              </CardTitle>
+              <CardDescription>
+                Manage your organization settings & members
+              </CardDescription>
+            </CardHeader>
+            <CardFooter>
+              <Link href="/dashboard/organization/general" className="w-full">
+                <Button className="w-full">
+                  Navigate to Settings
+                </Button>
+              </Link>
+            </CardFooter>
+          </Card>
         </div>
-        <Card className="mx-auto max-w-sm">
-          <CardHeader>
-            <CardTitle className="grid gap-2">
-              <span>Settings</span>
-            </CardTitle>
-            <CardDescription>
-              Manage your organization settings & members
-            </CardDescription>
-          </CardHeader>
-          <CardFooter>
-            <Link href="/dashboard/organization/general" className="w-full">
-              <Button className="w-full">
-                Navigate to Settings
-              </Button>
-            </Link>
-          </CardFooter>
-        </Card>
-        <Card className="mx-auto max-w-sm">
-          <CardHeader>
-            <CardTitle className="grid gap-2">
-              <span>Settings</span>
-            </CardTitle>
-            <CardDescription>
-              Manage your organization settings & members
-            </CardDescription>
-          </CardHeader>
-          <CardFooter>
-            <Link href="/dashboard/organization/general" className="w-full">
-              <Button className="w-full">
-                Navigate to Settings
-              </Button>
-            </Link>
-          </CardFooter>
-        </Card>
-        <Card className="mx-auto max-w-sm">
-          <CardHeader>
-            <CardTitle className="grid gap-2">
-              <span>Settings</span>
-            </CardTitle>
-            <CardDescription>
-              Manage your organization settings & members
-            </CardDescription>
-          </CardHeader>
-          <CardFooter>
-            <Link href="/dashboard/organization/general" className="w-full">
-              <Button className="w-full">
-                Navigate to Settings
-              </Button>
-            </Link>
-          </CardFooter>
-        </Card>
       </div>
     </div>
   )
