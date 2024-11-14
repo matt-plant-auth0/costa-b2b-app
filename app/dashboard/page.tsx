454f926d32b7d5bc5344419694df87d839d3be6c
Matt Plant <matthew.plant@okta.com>
2024-11-14 12:38:13 +0000

    added logic to display tiles based on role

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -14,7 +14,7 @@ import {
 export default async function DashboardHome() {
   const session = await appClient.getSession()
 
-  console.log(session?.user);
+  const isAdmin = session?.user['https://costa-b2b-app.vercel.app//roles'].includes('admin');
 
   return (
     <div className="flex flex-1 flex-grow flex-col gap-4 lg:gap-6">
@@ -25,59 +25,64 @@ export default async function DashboardHome() {
           </h3>
         </div>
         <div className="grid grid-flow-row-dense grid-cols-3 gap-12">
-          <Card className="bg-[#6D1F37] text-white max-w-[300px]">
-            <CardHeader>
-              <CardTitle className="grid gap-2">
-                <span>Reports</span>
-              </CardTitle>
-              <CardDescription>
-                View your buying history and reports
-              </CardDescription>
-            </CardHeader>
-            <CardFooter>
-              <Link target="_blank" href="https://okta.com" className="w-full">
-                <Button className="w-full">
-                  Navigate to Reports
-                  <OpenInNewWindowIcon className="ml-2 size-4" />
-                </Button>
-              </Link>
-            </CardFooter>
-          </Card>
-          <Card className="bg-[#6D1F37] text-white max-w-[300px]">
-            <CardHeader>
-              <CardTitle className="grid gap-2">
-                <span>Profisee</span>
-              </CardTitle>
-              <CardDescription>
-                Access your data in Profisee
-              </CardDescription>
-            </CardHeader>
-            <CardFooter>
-              <Link target="_blank" href="https://profisee.com/platform/" className="w-full">
-                <Button className="w-full">
-                  Navigate to Profisee
-                  <OpenInNewWindowIcon className="ml-2 size-4" />
-                </Button>
-              </Link>
-            </CardFooter>
-          </Card>
-          <Card className="bg-[#6D1F37] text-white max-w-[300px]">
-            <CardHeader>
-              <CardTitle className="grid gap-2">
-                <span>Settings</span>
-              </CardTitle>
-              <CardDescription>
-                Manage your organization settings
-              </CardDescription>
-            </CardHeader>
-            <CardFooter>
-              <Link href="/dashboard/organization/general" className="w-full">
-                <Button className="w-full">
-                  Navigate to Settings
-                </Button>
-              </Link>
-            </CardFooter>
-          </Card>
+          {!isAdmin && (
+            <>
+              <Card className="bg-[#6D1F37] text-white max-w-[300px]">
+                <CardHeader>
+                  <CardTitle className="grid gap-2">
+                    <span>Reports</span>
+                  </CardTitle>
+                  <CardDescription>
+                    View your buying history and reports
+                  </CardDescription>
+                </CardHeader>
+                <CardFooter>
+                  <Link target="_blank" href="https://okta.com" className="w-full">
+                    <Button className="w-full">
+                      Navigate to Reports
+                      <OpenInNewWindowIcon className="ml-2 size-4" />
+                    </Button>
+                  </Link>
+                </CardFooter>
+              </Card><Card className="bg-[#6D1F37] text-white max-w-[300px]">
+                  <CardHeader>
+                    <CardTitle className="grid gap-2">
+                      <span>Profisee</span>
+                    </CardTitle>
+                    <CardDescription>
+                      Access your data in Profisee
+                    </CardDescription>
+                  </CardHeader>
+                  <CardFooter>
+                    <Link target="_blank" href="https://profisee.com/platform/" className="w-full">
+                      <Button className="w-full">
+                        Navigate to Profisee
+                        <OpenInNewWindowIcon className="ml-2 size-4" />
+                      </Button>
+                    </Link>
+                  </CardFooter>
+                </Card>
+            </>
+          )}
+          {isAdmin && (
+            <Card className="bg-[#6D1F37] text-white max-w-[300px]">
+              <CardHeader>
+                <CardTitle className="grid gap-2">
+                  <span>Settings</span>
+                </CardTitle>
+                <CardDescription>
+                  Manage your organization settings
+                </CardDescription>
+              </CardHeader>
+              <CardFooter>
+                <Link href="/dashboard/organization/general" className="w-full">
+                  <Button className="w-full">
+                    Navigate to Settings
+                  </Button>
+                </Link>
+              </CardFooter>
+            </Card>
+          )}
         </div>
       </div>
     </div>
